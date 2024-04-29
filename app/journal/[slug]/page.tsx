4519c1b030b23931dcae10474e3505a57cf927f8
ashcbrd <cabardoash@gmail.com>
2024-04-29 14:28:36 +0800

    feat: add delete journal function

--- app/journal/[slug]/page.tsx
+++ app/journal/[slug]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import { MdDelete } from "react-icons/md";
 
 import { Button } from "@/components/ui/button";
 import response from "../../../data/journals.json";
@@ -9,6 +10,16 @@ import JournalClient from "@/components/journal-client";
 import { axiosInstance } from "@/lib/utils";
 import { useEffect, useState } from "react";
 import { usePublicRouteRedirect } from "@/hooks/use-auth-redirection";
+import { toast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 function getData(id: string) {
   const response = axiosInstance
@@ -26,6 +37,9 @@ function getData(id: string) {
 
 export default function JournalPage({ params }: { params: { slug: string } }) {
   const [journal, setJournal] = useState(null);
+  const router = useRouter();
+
+  const sample = response.data.find((item) => item._id === params);
 
   useEffect(() => {
     getData(params.slug).then((data) => {
@@ -36,20 +50,64 @@ export default function JournalPage({ params }: { params: { slug: string } }) {
 
   usePublicRouteRedirect();
 
+  function handleDeleteJournal(id: string) {
+    const response = axiosInstance
+      .delete(`/journal/${id}`, {})
+      .then((response) => {
+        let result = response;
+        toast({
+          description: "Journal deleted successfully.",
+        });
+        router.push("/journal");
+        return result;
+      })
+      .catch((error) => {
+        console.log("Error", error);
+      });
+
+    return response;
+  }
+
   return (
     <div>
-      <BackButtonClient variant="outline" />
+      <div className="w-full flex justify-between items-center">
+        <BackButtonClient variant="outline" />
+        <Dialog>
+          <DialogTrigger>
+            <Button variant="outline" className="py-6">
+              <MdDelete size={30} />
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>
+                Are you sure you want to delete this journal?
+              </DialogTitle>
+              <DialogDescription>
+                This action cannot be undone. This will permanently delete your
+                journal.
+              </DialogDescription>
+            </DialogHeader>
+            <Button
+              onClick={handleDeleteJournal(params.slug)}
+              className="w-max ml-auto"
+            >
+              Delete Journal
+            </Button>
+          </DialogContent>
+        </Dialog>
+      </div>
       <div className="flex flex-col w-full mt-10">
         <div className="flex w-full justify-between items-center">
-          <h2 className="font-semibold text-4xl">{journal?.title}</h2>
-          <Link href={`/message/student/${journal?.student_id}`}>
+          <h2 className="font-semibold text-4xl">{sample?.title}</h2>
+          <Link href={`/message/student/${sample?.student_id}`}>
             <Button className="px-10">Message</Button>
           </Link>
         </div>
-        <p className="mt-4">{journal?.entry}</p>
+        <p className="mt-4">{sample?.entry}</p>
       </div>
       {/* @ts-ignore */}
-      <JournalClient sentiments={journal?.sentiment_score} />
+      <JournalClient sentiments={sample?.sentiment_score} />
     </div>
   );
 }
