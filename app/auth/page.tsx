9d022e242e7df1b0bbe8ba1db120da0fc754bdfb
Asther Louie Cabardo <cabardoash@gmail.com>
2024-10-27 15:36:01 +0800

    Update page.tsx

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -10,7 +10,9 @@ const AuthPage = () => {
 
   return (
     <div className="flex flex-col gap-y-6 bg-white p-10 rounded-lg shadow-lg shadow-zinc-200/70">
-      <h1 className="text-center font-bold text-4xl">LOGO HERE</h1>
+     <h1 className="text-center text-[#1b1b1b] font-black text-4xl border px-4 py-2 rounded-lg bg-gray-100 shadow-md">
+        SMART JOURNAL
+      </h1>
       <Tabs defaultValue="login" className="w-[400px]">
         <TabsList className="w-full mb-10 bg-secondary">
           <TabsTrigger className="w-full" value="login">
